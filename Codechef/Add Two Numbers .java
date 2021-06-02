e5060478fb8a567d9530c1b50b777bd7bf00b7b6
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2021-06-02 19:43:24 +0530

    Create Add Two Numbers .java

---
+++ Codechef/Add Two Numbers .java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main{
+    private static final Scanner sc=new Scanner(System.in);
+    public static void main(String args[]){
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
+		
