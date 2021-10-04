9d6781ba2f1fb0dde7907c1bfc74d3e891b9f8f2
Koustav Ghosh <65316907+Koustav-coder@users.noreply.github.com>
2021-10-04 12:14:46 +0530

    Create Which Mixture.java

---
+++ Codechef/Which Mixture.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    if(A>0 && B>0)
+		    {
+		        System.out.println("Solution");
+		    }
+		    else if(A==0)
+		    {
+		        System.out.println("Liquid");
+		    }
+		    else if(B==0)
+		     {
+		        System.out.println("Solid");
+		    }
+		    
+		}
+	}
+}
